86c1070d150d2a9c6995e65ef7d73328cdb4e193
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-16 09:37:26 +0700

    create API get users

--- src/main/java/com/hiepstudy/hello_spring_boot/controller/UserController.java
+++ src/main/java/com/hiepstudy/hello_spring_boot/controller/UserController.java
@@ -4,20 +4,25 @@ import com.hiepstudy.hello_spring_boot.dto.request.UserCreationRequest;
 import com.hiepstudy.hello_spring_boot.entity.User;
 import com.hiepstudy.hello_spring_boot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping()
     User createUser(@RequestBody UserCreationRequest request){
         return userService.createRequest(request);
 
+    }
+
+    @GetMapping()
+    List<User> getUsers(){
+        return userService.getUsers();
     }
 
 }
--- src/main/java/com/hiepstudy/hello_spring_boot/service/UserService.java
+++ src/main/java/com/hiepstudy/hello_spring_boot/service/UserService.java
@@ -6,6 +6,8 @@ import com.hiepstudy.hello_spring_boot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
@@ -20,5 +22,9 @@ public class UserService {
         user.setDob(request.getDob());
 
         return userRepository.save(user);
+    }
+
+    public List<User> getUsers(){
+        return  userRepository.findAll();
     }
 }
